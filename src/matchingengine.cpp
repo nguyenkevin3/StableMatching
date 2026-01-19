56eca4bf0dd11b54b3af9fb612f44e4582b72173
nguyenkevin3 <148578177+nguyenkevin3@users.noreply.github.com>
2026-01-19 12:27:35 -0500

    Parsing input

--- src/matchingengine.cpp
+++ src/matchingengine.cpp
@@ -1,8 +1,69 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <direct.h>
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World! This is a test." << std::endl;
+    //User can drop a file into src and enter the filename
+    string inputFile;
+    cout << "Enter input file:" << endl;
+    getline(cin, inputFile);
+
+    ifstream file(inputFile);
+    //Check if failed to open file
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return -1;
+    }
+
+    //Credits to stack overflow "Checking for an empty file in C++"
+    if (file.peek() == ifstream::traits_type::eof()) {
+        cout << "Empty file!" << endl;
+        return -1;
+    }
+
+    //First line is integer n
+    int n;
+    file >> n;
+    if (n <= 0) {
+        cout << "Invalid integer!" << endl;
+        return -1;
+    }
+
+    //N hospitals, each with N students in their ranking lists
+    vector<vector<int>> hospitalPreference(n, vector<int>(n));
+    //Vice versa
+    vector<vector<int>> studentPreference(n, vector<int>(n));
+
+    //Read in hospital preference lists and make sure input is valid
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!(file >> hospitalPreference[i][j])) {
+                cout << "Invalid input!" << endl;
+                return -1;
+            }
+        }
+    }
+
+    //Read in student preference lists and make sure input is valid
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!(file >> studentPreference[i][j])) {
+                cout << "Invalid input!" << endl;
+                return -1;
+            }
+        }
+    }
+
+    int extra;
+    if (file >> extra) {
+        cout << "Invalid input!" << endl;
+        return -1;
+    }
+
+    file.close();
+
     return 0;
 }
